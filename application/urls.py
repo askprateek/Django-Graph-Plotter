ca206cb500a05ae8cf64c78cf650716e12a4cbba
askprateek <askpk123@gmail.com>
2016-07-05 00:37:37 +0530

    Static Files Fix

--- application/urls.py
+++ application/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('bokeh_app.urls') ),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
